a0574fd2d238448d2aa9a42643f077a353ebd0b2
carl.yu <carl.yu@carlyudeMacBook-Pro.local>
2020-11-10 10:24:10 +0800

    feat: eslint + prettier

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,38 @@
   <div id="app">
     hello world
 
-    <egg-button plain>plain</egg-button>
-    <egg-button type="primary">primary</egg-button>
-    <egg-button type="secondary">secondary</egg-button>
-    <egg-button type="info">info</egg-button>
+    <div class="col">
+      <egg-button>Default</egg-button>
+      <egg-button size="medium">default medium</egg-button>
+      <egg-button size="small">default small</egg-button>
+      <egg-button size="mini">default mini</egg-button>
+    </div>
+
+    <div class="col">
+      <egg-button type="primary">primary</egg-button>
+      <egg-button type="secondary">secondary</egg-button>
+      <egg-button type="text">text</egg-button>
+    </div>
   </div>
 </template>
 
 <script>
-    import {Button} from "./packages/Button";
+import { Button } from './packages/Button'
 
-    export default {
-        name: 'app',
-        components: {
-            EggButton: Button,
-        }
-    }
+export default {
+  name: 'app',
+  components: {
+    EggButton: Button,
+  },
+}
 </script>
 
 <style>
-  @import './assets/styles/variables.scss';
+@import './assets/styles/variables.scss';
 #app {
+  .col {
+    display: flex;
+    flex-direction: column;
+  }
 }
 </style>
--- src/packages/Button/src/Button.vue
+++ src/packages/Button/src/Button.vue
@@ -1,60 +1,104 @@
 <template>
-  <div :class="['egg-button', {'is-plain' : plain}, `egg-button--${type}`]">
+  <div
+    :class="[
+      'egg-button',
+      { disabled: disabled },
+      type ? `egg-button--${type}` : '',
+      size ? `egg-button--${size}` : '',
+    ]"
+  >
     <slot></slot>
   </div>
 </template>
 
 <script>
-    const EggButtonType = ['primary', 'secondary', 'info', 'text']
-    const EggButtonSize = ['large', 'medium', 'small', 'mini']
+const EggButtonType = ['primary', 'secondary', 'hollow', 'text']
+const EggButtonSize = ['medium', 'small', 'mini']
+const EggButtonSemantic = ['', 'success', 'warning', 'info', 'danger']
 
-    export default {
-        name: 'EggButton',
-        props: {
-            size: {
-                type: String,
-                default: 'large',
-                validator: (val) => {
-                    return EggButtonSize.includes(val)
-                }
-            },
-            // 不同颜色
-            type: {
-                type: String,
-                default: 'default',
-                validator: (val) => {
-                    return EggButtonType.includes(val)
-                }
-            },
-            plain: {
-                type: Boolean,
-                default: false,
-            },
-            disabled: {
-                type: Boolean,
-                default: false,
-            },
-            icon: {
-                type: String,
-            },
-        },
-    }
+export default {
+  name: 'EggButton',
+  props: {
+    size: {
+      type: String,
+      validator: (val) => {
+        return EggButtonSize.includes(val)
+      },
+    },
+    // 不同层级的按钮
+    type: {
+      type: String,
+      default: 'hollow',
+      validator: (val) => {
+        return EggButtonType.includes(val)
+      },
+    },
+    semantic: {
+      type: String,
+      validator: (val) => {
+        return EggButtonSemantic.includes(val)
+      },
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+    },
+  },
+}
 </script>
 
-<style lang="scss">
-  .egg-button {
-    border: 1px solid #6979F8;
+<style lang="scss" scoped>
+.egg-button {
+  padding: 11px 74px;
+  display: inline-block;
+  border-radius: 6px;
+  border: 1px solid #6979f8;
+  font-size: 17px;
+  font-family: Helvetica;
+  letter-spacing: 2px;
+  color: $--color-primary;
+  line-height: 22px;
+  cursor: pointer;
 
-    &.egg-button--default {
-      border-color: $--color-primary;
-    }
-
-    &.egg-button--primary {
-      background-color: $--color-primary;
-    }
-
-    &.egg-button--secondary {
-      background-color: $--color-primary-light-1;
-    }
+  // size
+  &.egg-button--medium {
+    padding: 10px 50px;
+    font-size: 14px;
   }
+  &.egg-button--small {
+    padding: 5px 20px;
+    font-size: 13px;
+  }
+  &.egg-button--mini {
+    padding: 4px;
+    font-size: 12px;
+  }
+
+  // type
+  &.egg-button--primary {
+    color: $--color-white;
+    background-color: $--color-primary;
+  }
+  &.egg-button--hollow {
+    border-width: 2px;
+    background-color: $--color-white;
+  }
+  &.egg-button--secondary {
+    border: none;
+    background-color: $--color-primary-lightest;
+  }
+  &.egg-button--text {
+    border: none;
+    background-color: unset;
+  }
+
+  &.egg-button--secondary {
+  }
+}
+
+@mixin eggButtonSize($size) {
+}
 </style>
