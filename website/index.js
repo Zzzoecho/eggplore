b0c87234f43d369a7b69510ef32a5d6af2af7b36
carl <sizhen.yu@duitang.com>
2020-11-19 19:43:55 +0800

    feat: add vuerouter

--- website/index.js
+++ website/index.js
@@ -1,10 +1,20 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import App from './App.vue';
 import Button from '../packages/Button';
 
+Vue.use(VueRouter);
+
 Vue.component(Button.name, Button);
 
+const route = new VueRouter({
+  mode: 'hash',
+  base: __dirname,
+});
+
 new Vue({
+  route,
   el: '#app',
   render: (h) => h(App),
 });
