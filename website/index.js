42af05eaaccad0a41cfca3e232f55b61eb26123f
Zzzoe <531575302@qq.com>
2021-03-11 21:13:58 +0800

    feat: Add Row

--- website/index.js
+++ website/index.js
@@ -3,10 +3,12 @@ import VueRouter from 'vue-router';
 
 import App from './App.vue';
 import Button from '../packages/Button';
+import Row from '../packages/Row';
 
 Vue.use(VueRouter);
 
 Vue.component(Button.name, Button);
+Vue.component(Row.name, Row);
 
 const route = new VueRouter({
   mode: 'hash',
