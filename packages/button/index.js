559e76f1f796a49f3009f08eaad675de30530791
Zzzoe <531575302@qq.com>
2021-03-13 15:10:27 +0800

    feat: rollup.config.js

--- packages/button/index.js
+++ packages/button/index.js
@@ -1,8 +1,8 @@
-import Button from './src/button.vue';
+import EggButton from './src/button.vue';
 
 // 插件写法 按需引入时用
-Button.install = function (Vue) {
-  Vue.component(Button.name, Button);
+EggButton.install = function (Vue) {
+  Vue.component(EggButton.name, EggButton);
 };
 
-export default Button;
+export default EggButton;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Button from '../packages/button/index';
+import EggRow from '../packages/row/index';
 
 const install = function (Vue) {
   Vue.component(Button.name, Button);
+  Vue.component(EggRow.name, EggRow);
 };
 
 if (typeof window !== 'undefined' && window.Vue) {
