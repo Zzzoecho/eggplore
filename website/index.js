7ebd5ee154a19bce07495ea4f1f00b0952b501b3
zzzoecho <531575302@qq.com>
2021-04-08 17:23:14 +0800

    add element-ui

--- website/index.js
+++ website/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue';
 import Eggplore from '../lib/index.umd';
 import '../lib/index.umd.css';
 
-
+Vue.use(ElementUI);
 Vue.use(VueRouter);
 Vue.use(Eggplore);
 
